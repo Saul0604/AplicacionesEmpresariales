327ec9e5dab9fd65ef6e74f36d52b3bb0445bf68
Saul0604 <sagd08@gmail.com>
2024-09-09 08:21:46 -0600

    actucalizacion practicas

--- practica5/js/index.js
+++ practica5/js/index.js
@@ -1,17 +1,46 @@
-"use strict";
-let miVariable = 'Hola mundo';
+var miVariable = 'Hola mundo';
 console.log(miVariable);
-const personay = {
+var personay = {
     nombrey: 'Lucas',
     apellidoy: 'Gomez',
     edady: 23
 };
-let { nombrey, apellidoy, edady = 24 } = personay;
+var nombrey = personay.nombrey, apellidoy = personay.apellidoy, _a = personay.edady, edady = _a === void 0 ? 24 : _a;
 console.log('nombre:', nombrey, 'apellido:', apellidoy, 'edad:', edady);
-let miNombre = 'saul';
-let [a1, a2, a3, a4] = miNombre;
+var miNombre = 'saul';
+var a1 = miNombre[0], a2 = miNombre[1], a3 = miNombre[2], a4 = miNombre[3];
 console.log(a1, a2, a3, a4); //s a u l
-const frutas = ['apple', 'orange', 'banana', 'pear'];
+var frutas = ['apple', 'orange', 'banana', 'pear'];
 //let [f1, f2, f3] = frutas;
-let [f1, , , f3] = frutas;
+var f1 = frutas[0], f3 = frutas[3];
 console.log(f1, f3);
+var Persona = /** @class */ (function () {
+    function Persona(nombrez, apellidoz, edadz) {
+        this.nombrez = nombrez;
+        this.apellidoz = apellidoz;
+        this.edadz = edadz;
+    }
+    Persona.prototype.getNombrez = function (nombrez) {
+        return this.nombrez;
+    };
+    Persona.prototype.setNombrez = function (nombrez) {
+        this.nombrez = nombrez;
+    };
+    Persona.prototype.getApellidoz = function (apellidoz) {
+        return this.apellidoz;
+    };
+    Persona.prototype.setApellidoz = function (apellidoz) {
+        this.apellidoz = apellidoz;
+    };
+    Persona.prototype.getEdadz = function (edadz) {
+        return this.edadz;
+    };
+    Persona.prototype.setEdadz = function (edadz) {
+        this.edadz = edadz;
+    };
+    return Persona;
+}());
+var alberto = new Persona('Alberto', 'Perez', 25);
+alberto.edadz = 20;
+console.log(alberto.nombrez, alberto.apellidoz, alberto.edadz);
+var pipe = new Persona('Pipe', 'Gomez', 23);
--- practica5/src/index.ts
+++ practica5/src/index.ts
@@ -17,4 +17,40 @@ console.log(a1, a2, a3, a4);    //s a u l
 const frutas = ['apple', 'orange', 'banana', 'pear'];
 //let [f1, f2, f3] = frutas;
 let [f1,,, f3] = frutas;
-console.log(f1,f3)
+console.log(f1,f3);
+
+class Persona {
+    nombrez: string;
+    apellidoz: string;
+    edadz: number;
+
+    constructor(nombrez: string, apellidoz: string, edadz: number) {
+        this.nombrez = nombrez;
+        this.apellidoz = apellidoz;
+        this.edadz = edadz;
+    }
+    getNombrez(nombrez: string) {
+        return this.nombrez;
+    }
+    setNombrez(nombrez: string) {
+        this.nombrez = nombrez;
+    }
+    getApellidoz(apellidoz: string) {
+        return this.apellidoz;
+    }
+    setApellidoz(apellidoz: string) {
+        this.apellidoz = apellidoz;
+    }
+    getEdadz(edadz: number) {
+        return this.edadz;
+    }
+    setEdadz(edadz: number) {
+        this.edadz = edadz;
+    }
+}
+
+let alberto = new Persona('Alberto', 'Perez', 25);
+alberto.edadz = 20;
+console.log(alberto.nombrez, alberto.apellidoz, alberto.edadz);
+
+let pipe = new Persona('Pipe', 'Gomez', 23);
